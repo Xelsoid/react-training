58a6153d2a9e822c2a55d756d1b126f05da48be1
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2020-06-09 10:57:41 +0300

    [task-4] feat: fix ErrorBoundary snapshot, add util that format incoming data, cover it by test

--- src/components/FilmDescription/index.test.js
+++ src/components/FilmDescription/index.test.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import MOCKED_DATA from '@mockedData/MOCKED_DATA';
+import { formatIncomingData } from '@utils';
 import ComponentExample from './index';
 
 describe('FilmDescription test', () => {
   const film = MOCKED_DATA.data[1];
-  const {
-    poster_path: posterPath,
-    title,
-    tagline,
-    vote_average: voteAverage,
-    release_date: releaseDate,
-    runtime,
-    overview,
-  } = film;
+  const defaultProps = formatIncomingData(film);
 
   it('renders correctly', () => {
-    const component = shallow(<ComponentExample
-      posterPath={posterPath}
-      title={title}
-      tagline={tagline}
-      voteAverage={voteAverage}
-      releaseDate={releaseDate}
-      runtime={runtime}
-      overview={overview}
-    />);
+    const component = shallow(<ComponentExample {...defaultProps} />);
     expect(component).toMatchSnapshot();
   });
 });
--- src/components/GalleryCard/index.test.js
+++ src/components/GalleryCard/index.test.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import MOCKED_DATA from '@mockedData/MOCKED_DATA';
+import { formatIncomingData } from '@utils';
 import ComponentExample from './index';
 
 describe('GalleryCard test', () => {
   const film = MOCKED_DATA.data[1];
-  const {
-    poster_path: posterPath,
-    title,
-    release_date: releaseDate,
-    genres,
-  } = film;
+  const defaultProps = formatIncomingData(film);
 
   it('renders correctly', () => {
-    const component = shallow(<ComponentExample
-      posterPath={posterPath}
-      title={title}
-      releaseDate={releaseDate}
-      genres={genres}
-    />);
+    const component = shallow(<ComponentExample {...defaultProps} />);
     expect(component).toMatchSnapshot();
   });
 });

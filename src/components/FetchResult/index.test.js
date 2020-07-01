78d02326d335b4da0b73d0870914029b0824ba39
Artsiom_Reutovich <artemrevtovich@gmail.com>
2020-07-01 14:07:42 +0300

    [task-5] feat: update the tests

--- src/components/FetchResult/index.test.js
+++ src/components/FetchResult/index.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { FETCH_HANDLERS } from '@root/src/services/constants';
 import ComponentExample from './index';
 
 describe('FetchResult test', () => {
-  it('renders correctly', () => {
-    const component = shallow(<ComponentExample />);
+  const ChildExample = () => (<h1> Child test component </h1>);
+
+  it('component doesnt render', () => {
+    const component = mount(<ComponentExample />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders correctly Loading', () => {
+    const component = mount(
+      <ComponentExample
+        loading={{ [FETCH_HANDLERS.MOVIE]: true }}
+        handlerId={FETCH_HANDLERS.MOVIE}
+      />,
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders correctly Error', () => {
+    const component = mount(
+      <ComponentExample
+        error={{ [FETCH_HANDLERS.MOVIE]: true }}
+        handlerId={FETCH_HANDLERS.MOVIE}
+      />,
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  it('renders correctly Child component', () => {
+    const component = mount(
+      <ComponentExample>
+        <ChildExample />
+      </ComponentExample>,
+    );
     expect(component).toMatchSnapshot();
   });
 });
--- src/services/movieReducers.test.js
+++ src/services/movieReducers.test.js
@@ -1,20 +1,14 @@
 import MOCKED_DATA from '@mockedData/MOCKED_DATA';
 import { sortByRating, sortByReleaseDate } from '@utils/index';
+import { ACTIONS } from '@root/src/services/constants';
 import {
-  reducers,
-  ACTIONS,
-  LOADINGS,
-  ERRORS,
   addMoviesDataToStore,
   addMovieDataToStore,
-  handleFetchErrors,
-  handleLoading,
-  fetchMoviesData,
-  fetchMovieData,
   clearMoviesDataFromStore,
   filterByReleaseDate,
   filterByRating,
-} from './reducers';
+  movieReducers,
+} from './movieReducers';
 
 describe('actions test', () => {
   it('action store MOVIES fetched data', () => {
@@ -31,54 +25,6 @@ describe('actions test', () => {
       {
         type: ACTIONS.STORE_MOVIE,
         payload: MOCKED_DATA.data[0],
-      },
-    );
-  });
-
-  it('action fetched data errors (flag: true)', () => {
-    expect(handleFetchErrors(ERRORS.MOVIES_ERROR, true)).toEqual(
-      {
-        type: ACTIONS.HANDLE_FETCH_ERRORS,
-        payload: {
-          id: ERRORS.MOVIES_ERROR,
-          error: true,
-        },
-      },
-    );
-  });
-
-  it('action fetched data errors (flag: false)', () => {
-    expect(handleFetchErrors(ERRORS.MOVIES_ERROR, false)).toEqual(
-      {
-        type: ACTIONS.HANDLE_FETCH_ERRORS,
-        payload: {
-          id: ERRORS.MOVIES_ERROR,
-          error: false,
-        },
-      },
-    );
-  });
-
-  it('action fetched data loading (flag: true)', () => {
-    expect(handleLoading(LOADINGS.MOVIE_LOADING, true)).toEqual(
-      {
-        type: ACTIONS.HANDLE_FETCH_LOADING,
-        payload: {
-          id: LOADINGS.MOVIE_LOADING,
-          flag: true,
-        },
-      },
-    );
-  });
-
-  it('action fetched data loading (flag: false)', () => {
-    expect(handleLoading(LOADINGS.MOVIE_LOADING, false)).toEqual(
-      {
-        type: ACTIONS.HANDLE_FETCH_LOADING,
-        payload: {
-          id: LOADINGS.MOVIE_LOADING,
-          flag: false,
-        },
       },
     );
   });
@@ -107,13 +53,14 @@ describe('actions test', () => {
     );
   });
 });
+
 describe('reducers tests', () => {
   const initialState = {
     moviesData: MOCKED_DATA,
   };
 
   it('reducers should return the initial state', () => {
-    expect(reducers(initialState, {})).toEqual(initialState);
+    expect(movieReducers(initialState, {})).toEqual(initialState);
   });
 
   it('should handle STORE_MOVIES', () => {
@@ -121,7 +68,7 @@ describe('reducers tests', () => {
       type: ACTIONS.STORE_MOVIES,
       payload: MOCKED_DATA,
     };
-    expect(reducers(initialState, action)).toEqual({ ...initialState, moviesData: action.payload });
+    expect(movieReducers(initialState, action)).toEqual({ ...initialState, moviesData: action.payload });
   });
 
   it('should handle STORE_MOVIE', () => {
@@ -129,55 +76,21 @@ describe('reducers tests', () => {
       type: ACTIONS.STORE_MOVIE,
       payload: MOCKED_DATA.data[0],
     };
-    expect(reducers(initialState, action)).toEqual({ ...initialState, movieData: action.payload });
+    expect(movieReducers(initialState, action)).toEqual({ ...initialState, movieData: action.payload });
   });
 
   it('should handle CLEAR_STORE_MOVIES', () => {
     const action = {
       type: ACTIONS.CLEAR_STORE_MOVIES,
     };
-    expect(reducers(initialState, action)).toEqual({ ...initialState, moviesData: {} });
-  });
-
-  it('should handle HANDLE_FETCH_ERRORS', () => {
-    const action = {
-      type: ACTIONS.HANDLE_FETCH_ERRORS,
-      payload: {
-        id: ERRORS.MOVIES_ERROR,
-        error: false,
-      },
-    };
-    expect(reducers(initialState, action)).toEqual({
-      ...initialState,
-      errors: {
-        ...initialState.errors,
-        [action.payload.id]: action.payload.error,
-      },
-    });
-  });
-
-  it('should handle HANDLE_FETCH_LOADING', () => {
-    const action = {
-      type: ACTIONS.HANDLE_FETCH_LOADING,
-      payload: {
-        id: LOADINGS.MOVIE_LOADING,
-        flag: true,
-      },
-    };
-    expect(reducers(initialState, action)).toEqual({
-      ...initialState,
-      loading: {
-        ...initialState.loading,
-        [action.payload.id]: action.payload.flag,
-      },
-    });
+    expect(movieReducers(initialState, action)).toEqual({ ...initialState, moviesData: {} });
   });
 
   it('should handle FILTER_BY_RELEASE_DATE', () => {
     const action = {
       type: ACTIONS.FILTER_BY_RELEASE_DATE,
     };
-    expect(reducers(initialState, action))
+    expect(movieReducers(initialState, action))
       .toEqual({
         ...initialState,
         moviesData:
@@ -192,7 +105,7 @@ describe('reducers tests', () => {
     const action = {
       type: ACTIONS.FILTER_BY_RATING,
     };
-    expect(reducers(initialState, action)).toEqual({
+    expect(movieReducers(initialState, action)).toEqual({
       ...initialState,
       moviesData:
         { ...initialState.moviesData, data: sortByRating(initialState.moviesData.data) },
